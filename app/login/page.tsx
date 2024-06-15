1e14629d95b16298c31a9dda36a27d05b6ec2bf5
yachzu <ashilcontact@gmail.com>
2024-06-16 03:02:17 +0700

    i

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,17 +1,33 @@
-import AcmeLogo from '@/app/ui/acme-logo';
-import LoginForm from '@/app/ui/login-form';
+'use client'
  
-export default function LoginPage() {
+import { authenticate } from '@/app/lib/actions'
+import { useFormState, useFormStatus } from 'react-dom'
+ 
+export default function Page() {
+  const [errorMessage, dispatch] = useFormState(authenticate, undefined)
+ 
   return (
-    <main className="flex items-center justify-center md:h-screen">
-      <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
-        <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
-          <div className="w-32 text-white md:w-36">
-            <AcmeLogo />
-          </div>
-        </div>
-        <LoginForm />
-      </div>
-    </main>
-  );
+    <form action={dispatch}>
+      <input type="email" name="email" placeholder="Email" required />
+      <input type="password" name="password" placeholder="Password" required />
+      <div>{errorMessage && <p>{errorMessage}</p>}</div>
+      <LoginButton />
+    </form>
+  )
+}
+ 
+function LoginButton() {
+  const { pending } = useFormStatus()
+ 
+  const handleClick = (event) => {
+    if (pending) {
+      event.preventDefault()
+    }
+  }
+ 
+  return (
+    <button aria-disabled={pending} type="submit" onClick={handleClick}>
+      Login
+    </button>
+  )
 }
