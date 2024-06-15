311cd18734cdaa590db865362f84071135d175c0
yachzu <ashilcontact@gmail.com>
2024-06-16 04:14:48 +0700

    u

--- app/lib/middleware.ts
+++ app/lib/middleware.ts
@@ -1,16 +1,17 @@
-import type { NextRequest } from 'next/server'
- 
+import type { NextRequest } from 'next/server';
+
 export function middleware(request: NextRequest) {
   const currentUser = request.cookies.get('currentUser')?.value;
-  console.log('Current User:', currentUser); // Tambahkan ini untuk debugging
 
   if (currentUser && !request.nextUrl.pathname.startsWith('/dashboard')) {
-    console.log('Redirecting to /dashboard'); // Tambahkan ini untuk debugging
     return Response.redirect(new URL('/dashboard', request.url));
   }
 
   if (!currentUser && !request.nextUrl.pathname.startsWith('/login')) {
-    console.log('Redirecting to /login'); // Tambahkan ini untuk debugging
     return Response.redirect(new URL('/login', request.url));
   }
 }
+
+export const config = {
+  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+};
