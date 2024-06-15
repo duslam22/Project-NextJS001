89feec69611d0ae4c741d06589b7b09778ee8ac7
yachzu <ashilcontact@gmail.com>
2024-06-16 03:18:20 +0700

    u

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,33 +1,17 @@
-'use client'
+import AcmeLogo from '@/app/ui/acme-logo';
+import LoginForm from '@/app/ui/login-form';
  
-import { authenticate } from '@/app/lib/actions'
-import { useFormState, useFormStatus } from 'react-dom'
- 
-export default function Page() {
-  const [errorMessage, dispatch] = useFormState(authenticate, undefined)
- 
+export default function LoginPage() {
   return (
-    <form action={dispatch}>
-      <input type="email" name="email" placeholder="Email" required />
-      <input type="password" name="password" placeholder="Password" required />
-      <div>{errorMessage && <p>{errorMessage}</p>}</div>
-      <LoginButton />
-    </form>
-  )
-}
- 
-function LoginButton() {
-  const { pending } = useFormStatus()
- 
-  const handleClick = (event: { preventDefault: () => void }) => {
-    if (pending) {
-      event.preventDefault()
-    }
-  }
- 
-  return (
-    <button aria-disabled={pending} type="submit" onClick={handleClick}>
-      Login
-    </button>
-  )
+    <main className="flex items-center justify-center md:h-screen">
+      <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
+        <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
+          <div className="w-32 text-white md:w-36">
+            <AcmeLogo />
+          </div>
+        </div>
+        <LoginForm />
+      </div>
+    </main>
+  );
 }
