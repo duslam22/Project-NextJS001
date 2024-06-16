a03ccc4177afd970a6237a80aad49632cf4f22e0
duslam22 <mamahga18@gmail.com>
2024-06-17 01:45:53 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,6 @@ import Image from 'next/image';
 import { redirect } from 'next/navigation'
  
 export default function Page() {
-  // Logic to determine if a redirect is needed
-  const accessDenied = true
-  if (accessDenied) {
-    redirect('/login')
-  }
   return (
     <div className="min-h-screen flex flex-col">
       {/* Header */}
@@ -100,4 +95,26 @@ export default function Page() {
       </footer>
     </div>
   );
-}  
+}
+
+
+// import AcmeLogo from '@/app/ui/acme-logo';
+// import { ArrowRightIcon } from '@heroicons/react/24/outline';
+// import Link from 'next/link';
+// import Image from 'next/image';
+// export default function Page() {
+//   return (
+//     // ...
+//     <div className="md:py-12 flex items-center justify-center p-6 md:w-3/5 md:px-28">
+//       {/* Add Hero Images Here */}
+//       <Image
+//         src="/hero-desktop.png"
+//         width={1000}
+//         height={760}
+//         className="hidden md:block"
+//         alt="Screenshots of the dashboard project showing desktop version"
+//       />
+//     </div>
+//     //...
+//   );
+// }
