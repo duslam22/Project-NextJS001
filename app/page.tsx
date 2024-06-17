20b4ce03bf4a6cc38d061ef663bd86410012c857
duslam22 <mamahga18@gmail.com>
2024-06-18 04:31:07 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,7 @@ import Image from 'next/image';
 import { redirect } from 'next/navigation'
  
 export default function Page() {
-  // Logic to determine if a redirect is needed
-  const accessDenied = true
-  if (accessDenied) {
-    redirect('/login')
-  }
+ 
  
   // Define other routes and logic
 }
