202f5c34aafbef5e33e86dd2c783dcd3b1413ee0
duslam22 <mamahga18@gmail.com>
2024-06-18 04:55:57 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import Pagination from '@/app/ui/invoices/pagination';
 import Search from '@/app/ui/search';
 import Table from '@/app/ui/invoices/table';
-import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
 import { fetchInvoicesPages } from '@/app/lib/data';
@@ -22,18 +21,38 @@ export default async function Page({
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
-        <h1 className={`text-2xl`}>Invoices</h1>
+        <h1 className="text-2xl">Invoices</h1>
       </div>
       <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
         <Search placeholder="Search invoices..." />
-        <CreateInvoice />
       </div>
       <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
         <Table query={query} currentPage={currentPage} />
       </Suspense>
       <div className="mt-5 flex w-full justify-center">
-      <Pagination totalPages={totalPages} />
+        <Pagination totalPages={totalPages} />
       </div>
     </div>
   );
 }
+
+// import AcmeLogo from '@/app/ui/acme-logo';
+// import { ArrowRightIcon } from '@heroicons/react/24/outline';
+// import Link from 'next/link';
+// import Image from 'next/image';
+// export default function Page() {
+//   return (
+//     // ...
+//     <div className="md:py-12 flex items-center justify-center p-6 md:w-3/5 md:px-28">
+//       {/* Add Hero Images Here */}
+//       <Image
+//         src="/hero-desktop.png"
+//         width={1000}
+//         height={760}
+//         className="hidden md:block"
+//         alt="Screenshots of the dashboard project showing desktop version"
+//       />
+//     </div>
+//     //...
+//   );
+// }
