4c0f52c6700e30f71b43bd7b71c2bf125d416c3b
duslam22 <mamahga18@gmail.com>
2024-06-18 07:44:44 +0700

    update

--- app/lib/placeholder-data.js
+++ app/lib/placeholder-data.js
@@ -11,66 +11,67 @@ const users = [
 
 const customers = [
   {
-    id: '3958dc9e-712f-4377-85e9-fec4b6a6442a',
+    id: 1,
     name: 'Es Teh',
     email: 'esteh@toko.com',
     image_url: '/customers/esteh.png',
   },
   {
-    id: '3958dc9e-742f-4377-85e9-fec4b6a6442a',
+    id: 2,
     name: 'Teh Anget',
     email: 'tehanget@toko.com',
     image_url: '/customers/tehpan.png',
   },
   {
-    id: '3958dc9e-737f-4377-85e9-fec4b6a6442a',
+    id: 3,
     name: 'Kopi Susu',
     email: 'kopisusu@toko.com',
     image_url: '/customers/kopis.png',
   },
   {
-    id: '50ca3e18-62cd-11ee-8c99-0242ac120002',
+    id: 4,
     name: 'Kopi Hitam',
     email: 'kopihitam@tey.com',
     image_url: '/customers/kopih.png',
   },
   {
-    id: '3958dc9e-787f-4377-85e9-fec4b6a6442a',
+    id: 5,
     name: 'Magelangan',
     email: 'magelangan@toko.com',
     image_url: '/customers/magelangan.png',
   },
   {
-    id: '76d65c26-f784-44a2-ac19-586678f7c2f2',
+    id: 6,
     name: 'Mi goreng',
     email: 'migoreng@toko.com',
     image_url: '/customers/migo.png',
   },
   {
-    id: 'd6e15727-9fe1-4961-8c5b-ea44a9bd81aa',
+    id: 7,
     name: 'Mi kuah',
     email: 'mikuah@toko.com',
     image_url: '/customers/miku.png',
   },
   {
-    id: '126eed9c-c90c-4ef6-a4a8-fcf7408d3c66',
+    id: 8,
     name: 'Nasi Goreng',
     email: 'nasgor@toko.com',
     image_url: '/customers/nasgor.png',
   },
   {
-    id: 'CC27C14A-0ACF-4F4A-A6C9-D45682C144B9',
+    id: 9,
     name: 'Jus Aplukat',
     email: 'alpukat@toko.com',
     image_url: '/customers/aplukat.png',
   },
   {
-    id: '13D07535-C59E-4157-A011-F8D2EF4E0CBB',
+    id: 10,
     name: 'Jahe Susu',
     email: 'jasu@toko.com',
     image_url: '/customers/jasu.png',
   },
 ];
+
 
 const invoices = [
   {
